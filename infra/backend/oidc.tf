763dff20eadedfb748d4bb87ff189b5fbb5c6df0
tatan461 <jsporting777@gmail.com>
2026-09-21 18:08:26 +0200

    fix: recreate GitHub OIDC provider using dynamic thumbprint from tls_certificate

--- infra/backend/oidc.tf
+++ infra/backend/oidc.tf
@@ -10,10 +10,14 @@ variable "github_repo" {
   default     = "cloud-resume-ai"
 }
 
+data "tls_certificate" "github_actions" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = [data.tls_certificate.github_actions.certificates[0].sha1_fingerprint]
 }
 
 data "aws_iam_policy_document" "github_actions_trust" {
